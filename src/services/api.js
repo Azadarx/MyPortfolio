5fa4bfa4d6bb0fccd4c19ce3a48e6660610b6820
Syed Azadar Hussain <syedazadarhussayn@gmail.com>
2025-12-12 18:54:40 +0530

    Updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Read backend URL from .env
-const BASE_URL = import.meta.env.VITE_BACKEND_URL + "/api";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
